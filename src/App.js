61a7c5a42d961b1435138ffe17e1fed5d7b52d48
Ebram-Tooma <ebramtooma@gmail.com>
2022-03-11 04:38:07 +0200

    whoweare-termsofuse-privacypolicy

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import Categories from './pages/Categories';
 import Search from './pages/Search';
 import CategoryDetails from './pages/CategoryDetails' ;
 import Whoweare from './pages/Whoweare';
+import Privacypolicy from './pages/Privacypolicy';
+import TermsOfUse from './pages/TermsOfUse';
+
 
 // import Images from './Images';
 // import {Privacypolicy} from './pages/Privacypolicy'
@@ -39,7 +42,7 @@ function App() {
   return (
     <AuthProvider>
        <Navbar />
-       <div className='container'>
+       <div >
       <Routes>
     
       <Route path="/"     exact element={<Home/>} />
@@ -66,6 +69,8 @@ function App() {
       <Route path="/search/:resulte" exact element={<Search />} />
       <Route path="/subscription/:id" exact element={<Subscription/>} />
       <Route path={"/details/:id?"} exact component={CategoryDetails} />
+      <Route path="/privacypolicy" exact element={<Privacypolicy />} />
+      <Route path="/termsofuse" exact element={<TermsOfUse/>} />
       </Routes>
       </div>
       <ToastContainer />
